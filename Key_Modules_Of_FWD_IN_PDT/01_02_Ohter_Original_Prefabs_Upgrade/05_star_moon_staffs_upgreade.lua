e9da2e2a50a538f7e4509294340a63f7afb4be04
UnderDarkNight <under.dark.night@outlook.com>
2023-09-28 12:28:56 +0800

    05_star_moon_staffs_upgreade.lua  bug fix

--- Key_Modules_Of_FWD_IN_PDT/01_02_Ohter_Original_Prefabs_Upgrade/05_star_moon_staffs_upgreade.lua
+++ Key_Modules_Of_FWD_IN_PDT/01_02_Ohter_Original_Prefabs_Upgrade/05_star_moon_staffs_upgreade.lua
@@ -10,50 +10,45 @@ local Calling_Stars_Staff = "yellowstaff"
 local Calling_Moon_Staff = "opalstaff"
 local function staff_upgrade(inst)
         inst:AddComponent("fwd_in_pdt_data")
-        local old_fn = inst.components.spellcaster.spell
-        inst.components.spellcaster.spell = function(inst,target,pos,doer)
-            old_fn(inst,target,pos,doer)
 
-            if doer  and doer:HasTag("player") and doer.components.playercontroller then
-                local Probability =  TUNING["Forward_In_Predicament.Config"].Element_Core_Probability  or 0.1
-                if math.random(1000)/1000 <= Probability or TUNING.FWD_IN_PDT_MOD___DEBUGGING_MODE then
-                            -- local reward_prefab_name = "fwd_in_pdt_item_ice_core"
-                            -- local reward_prefab_name = "fwd_in_pdt_item_flame_core"
-                            local reward_prefab_name = nil
-                            if inst.prefab == Calling_Stars_Staff then
-                                reward_prefab_name = "fwd_in_pdt_item_flame_core"
-                            elseif inst.prefab == Calling_Moon_Staff then
-                                reward_prefab_name = "fwd_in_pdt_item_ice_core"
-                            end
-                            if reward_prefab_name then
-                                doer.components.inventory:GiveItem(SpawnPrefab(reward_prefab_name))
-                            end
-                            inst.components.fwd_in_pdt_data:Set("rewarded_flag",true) --- 保底标记位
+        --------------- 
+            local function GiveItem2Player(inst,player)
+                local reward_prefab_name = nil
+                if inst.prefab == Calling_Stars_Staff then
+                    reward_prefab_name = "fwd_in_pdt_item_flame_core"
+                elseif inst.prefab == Calling_Moon_Staff then
+                    reward_prefab_name = "fwd_in_pdt_item_ice_core"
+                end
+                if reward_prefab_name then
+                    player.components.inventory:GiveItem(SpawnPrefab(reward_prefab_name))
                 end
             end
+        --------------- 法杖施法的时候
+            local old_fn = inst.components.spellcaster.spell
+            inst.components.spellcaster.spell = function(inst,target,pos,doer)
+                old_fn(inst,target,pos,doer)
 
-        end
+                if doer  and doer:HasTag("player") and doer.components.playercontroller then
+                    local Probability =  TUNING["Forward_In_Predicament.Config"].Element_Core_Probability  or 0.1
+                    if math.random(1000)/1000 <= Probability or TUNING.FWD_IN_PDT_MOD___DEBUGGING_MODE then
+                                -- local reward_prefab_name = "fwd_in_pdt_item_ice_core"
+                                -- local reward_prefab_name = "fwd_in_pdt_item_flame_core"
+                                GiveItem2Player(inst,doer)
+                                inst.components.fwd_in_pdt_data:Set("rewarded_flag",true) --- 保底标记位
+                    end
+                end
 
+            end
         --------------- 保底奖励，耐久度用完，武器要被删的时候检查
             if inst.components.finiteuses then
 
                                 local old_finish_fn = inst.components.finiteuses.onfinished or nil
                                 inst.components.finiteuses.onfinished = function(inst)
                                     
-                                    if not inst.components.fwd_in_pdt_data:Set("rewarded_flag",true) then
+                                    if not inst.components.fwd_in_pdt_data:Get("rewarded_flag") then
                                         local owner = inst.components.inventoryitem:GetGrandOwner()
                                         if owner then
-
-                                                local reward_prefab_name = nil
-                                                if inst.prefab == Calling_Stars_Staff then
-                                                    reward_prefab_name = "fwd_in_pdt_item_flame_core"
-                                                elseif inst.prefab == Calling_Moon_Staff then
-                                                    reward_prefab_name = "fwd_in_pdt_item_ice_core"
-                                                end
-                                                if reward_prefab_name then
-                                                    owner.components.inventory:GiveItem(SpawnPrefab(reward_prefab_name))
-                                                end
-
+                                                GiveItem2Player(inst,owner)
                                         end
                                     end
 
