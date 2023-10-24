f5a170de12de6b9e289b3a6a4a76a48f59827303
UnderDarkNight <under.dark.night@outlook.com>
2023-10-24 17:31:27 +0800

    rpc log test

--- Key_Modules_Of_FWD_IN_PDT/03_Auxiliary_File_Registration_System/01_RPC_register.lua
+++ Key_Modules_Of_FWD_IN_PDT/03_Auxiliary_File_Registration_System/01_RPC_register.lua
@@ -43,9 +43,9 @@
 ---------------------------------------------------------------------------------------------------------------------------------
 -------- 跨存档数据传送
     AddClientModRPCHandler(TUNING["Forward_In_Predicament.RPC_NAMESPACE"],"cross_archived_data.server2client",function(data_json)
-        if  TUNING.FWD_IN_PDT_MOD___DEBUGGING_MODE then
+        -- if  TUNING.FWD_IN_PDT_MOD___DEBUGGING_MODE then
             print("RPC cross_archived_data.server2client",data_json)
-        end
+        -- end
         local crash_flag,data_table = pcall(json.decode,data_json)
         if crash_flag and ThePlayer then
             ThePlayer:PushEvent("fwd_in_pdt_event.Set_Cross_Archived_Data_To_Client",data_table)
@@ -56,9 +56,9 @@
     AddModRPCHandler(TUNING["Forward_In_Predicament.RPC_NAMESPACE"], "cross_archived_data.client2server", function(player_inst,data_json) ----- Register on the server
         -- user in client : inst.replica.fwd_in_pdt_func:PushEvent("event_name",data)
         -- 客户端回传 event 给 服务端,player_inst 为来源玩家客户端。
-        if  TUNING.FWD_IN_PDT_MOD___DEBUGGING_MODE then
+        -- if  TUNING.FWD_IN_PDT_MOD___DEBUGGING_MODE then
             print("RPC cross_archived_data.client2server",player_inst,data_json)
-        end
+        -- end
         if player_inst and data_json then
             local crash_flag ,data_table = pcall(json.decode,data_json)
             if crash_flag then
