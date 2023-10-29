2eae10c0e1e6cc3ece57f6e0f576164272de1a28
UnderDarkNight <under.dark.night@outlook.com>
2023-10-29 14:13:26 +0800

    componentactions CollectActions crash fix

--- Imports_for_FWD_IN_PDT/08_componentactions_crash_fix.lua
+++ Imports_for_FWD_IN_PDT/08_componentactions_crash_fix.lua
@@ -1,4 +1,6 @@
-
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--- 强制修一些 componentactions.lua 里 崩溃。至于为什么崩溃，不知道。
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -13,12 +15,33 @@
 -- end
 
 if GLOBAL.EntityScript.UnregisterComponentActions_fwd_in_pdt_old == nil then
-    rawset(GLOBAL.EntityScript,"UnregisterComponentActions_fwd_in_pdt_old",rawget(GLOBAL.EntityScript,"UnregisterComponentActions"))
-    rawset(GLOBAL.EntityScript, "UnregisterComponentActions", function(self,...)
-            -- print("fwd_in_pdt_test UnregisterComponentActions",self,...)
-        local crash_flg = pcall(self.UnregisterComponentActions_fwd_in_pdt_old,...)
-        if not crash_flg then
-            print("fwd_in_pdt error : UnregisterComponentActions",self,...)
-        end
-    end)
+
+
+    -------------------------------------------------------------------------------------------
+    ---- UnregisterComponentActions
+        rawset(GLOBAL.EntityScript,"UnregisterComponentActions_fwd_in_pdt_old",rawget(GLOBAL.EntityScript,"UnregisterComponentActions"))
+        rawset(GLOBAL.EntityScript, "UnregisterComponentActions", function(self,...)
+                -- print("fwd_in_pdt_test UnregisterComponentActions",self,...)
+            local crash_flg = pcall(self.UnregisterComponentActions_fwd_in_pdt_old,...)
+            if not crash_flg then
+                print("fwd_in_pdt error : UnregisterComponentActions",self,...)
+            end
+        end)
+    -------------------------------------------------------------------------------------------
+    ---- CollectActions
+        rawset(GLOBAL.EntityScript,"CollectActions_fwd_in_pdt_old",rawget(GLOBAL.EntityScript,"CollectActions"))
+        rawset(GLOBAL.EntityScript, "CollectActions", function(self,...)
+                -- print("fwd_in_pdt_test CollectActions",self,...)
+            local crash_flg = pcall(self.CollectActions_fwd_in_pdt_old,...)
+            if not crash_flg then
+                print("fwd_in_pdt error : CollectActions",self,...)
+            end
+        end)
+
+
+
+
+
+
+
 end
