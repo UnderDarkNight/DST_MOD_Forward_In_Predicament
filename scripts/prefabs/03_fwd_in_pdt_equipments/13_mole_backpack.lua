94961a5bba8ea049b5b684dab5321f611b9725c2
unknown <under.dark.night@outlook.com>
2023-12-15 22:56:19 +0800

    mole backpack slot bug fix

--- scripts/prefabs/03_fwd_in_pdt_equipments/13_mole_backpack.lua
+++ scripts/prefabs/03_fwd_in_pdt_equipments/13_mole_backpack.lua
@@ -167,7 +167,7 @@ local function fn()
     -- inst.components.inventoryitem.cangoincontainer = false
 
     inst:AddComponent("equippable")
-    inst.components.equippable.equipslot = EQUIPSLOTS.BACKPACK or EQUIPSLOTS.BODY
+    inst.components.equippable.equipslot = EQUIPSLOTS.BACK or EQUIPSLOTS.BEIBAO or EQUIPSLOTS.BAG or EQUIPSLOTS.BODY
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
     inst.components.equippable:SetOnEquipToModel(onequiptomodel)
