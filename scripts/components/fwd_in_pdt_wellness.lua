92746e58005c4515a8de6ff6e7d5b4e95ee19aa1
unknown <under.dark.night@outlook.com>
2024-03-12 21:44:49 +0800

    TUNING[Forward_In_Predicament.Config].BUILD_MOD bug fix

--- scripts/components/fwd_in_pdt_wellness.lua
+++ scripts/components/fwd_in_pdt_wellness.lua
@@ -411,6 +411,9 @@ nil,
 ------------------------------------------------------------------------------------------------------------------------------
 ---- 单个debuff 的添加删除 等操作
     function fwd_in_pdt_wellness:Add_Debuff(str)    --- 添加debuff
+        if TUNING["Forward_In_Predicament.Config"].BUILD_MOD then
+            return
+        end
         if type(str) == "string" and PrefabExists(str) then
                 if self.debuffs[str] == nil then    --- 没有这个debuff
                     local child = self.inst:SpawnChild(str)
