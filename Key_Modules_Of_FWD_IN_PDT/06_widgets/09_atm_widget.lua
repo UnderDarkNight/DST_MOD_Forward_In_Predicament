b709d9458e3d9d777bd423d8e1be886312dacde5
unknown <under.dark.night@outlook.com>
2025-01-30 18:47:53 +0800

    zip cdkey api added

--- Key_Modules_Of_FWD_IN_PDT/06_widgets/09_atm_widget.lua
+++ Key_Modules_Of_FWD_IN_PDT/06_widgets/09_atm_widget.lua
@@ -234,7 +234,7 @@ AddClassPostConstruct("screens/playerhud",function(self)
             cd_key_input_background:Hide()
             ------------------------------------------------------------
                 ----- text_box
-                    local text_box = cd_key_input_background:AddChild(TextEdit(CODEFONT,40,"[637,68...02,848]",{ 255/255 , 255/255 ,255/255 , 1}))
+                    local text_box = cd_key_input_background:AddChild(TextEdit(CODEFONT,40,"CDK:#####",{ 255/255 , 255/255 ,255/255 , 1}))
                     text_box:SetAllowNewline(false)
                     text_box:SetForceEdit(true)
                     text_box:EnableWordWrap(true)
@@ -272,7 +272,7 @@ AddClassPostConstruct("screens/playerhud",function(self)
                     cd_key_input_enter_button:SetOnDown(function()
                         local the_input_text = tostring(text_box:GetLineEditString())
                         -- print("input cd-key",the_input_text,the_input_text == "XXXX-XXXX-XXXX-XXXX")
-                        if the_input_text == "[637,68...02,848]" then
+                        if the_input_text == "CDK:#####" then
                             -- print("error with default input cd-key   XXXX-XXXX-XXXX-XXXX")
                         else
                             -- ThePlayer.replica.fwd_in_pdt_func:RPC_PushEvent("fwd_in_pdt_event.atm_enter_cd_key",the_input_text)
@@ -302,7 +302,7 @@ AddClassPostConstruct("screens/playerhud",function(self)
             cd_key_button:SetOnDown(function()
                 -- ThePlayer.replica.fwd_in_pdt_func:RPC_PushEvent2()
                 cd_key_input_background:Show()
-                text_box:SetString("[637,68...02,848]")
+                text_box:SetString("CDK:#####")
                 cd_key_button:Disable()
                 self.fwd_in_pdt_atm_widget_inst:DoTaskInTime(3,function()
                     cd_key_button:Enable()
